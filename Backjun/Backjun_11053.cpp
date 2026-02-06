753e1511b8e1c8d893419af2f194a4ab4f61f467
Lyeol2 <frog100f@gmail.com>
2026-02-07 01:37:38 +0900

    [ADD] DP문제 추가욧

---
+++ Backjun/Backjun_11053.cpp
@@ -0,0 +1,40 @@
+﻿//https://www.acmicpc.net/problem/11053
+// 가장 긴 증가하는 부분 수열
+#include <iostream>
+using namespace std;
+
+int N = 0;
+int arr[1000] = { 0, };
+
+// 시작 수열의 인덱스 / 길이
+// 
+int dpCount[1000];
+int dpChained[1000];
+int main()
+{
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+	dpCount[0] = 1;
+	// 시작수열보다 무조건 뒷수열이 길이가 길다
+	for (int i = 0; i < N; i++)
+	{
+
+		// 뒷수열 이 더 큰 숫자라면
+		if(arr[i] < arr[i + 1])
+		{
+			dpChained[i + 1] = dpChained[i];
+			dpCount[i + 1] = dpCount[i] + 1;
+		}
+		else
+		{
+			dpCount[i + 1] = 1;
+		}
+	}
+
+
+	return 0;
+}
---
+++ Backjun/Backjun_2156.cpp
@@ -0,0 +1,69 @@
+﻿// https://www.acmicpc.net/problem/2156
+// 포도주 시식 - 실버 1
+// 연속 3잔은 ㅁ마실수없고
+// 마신후엔 모두 마셔야한다
+// 661166
+
+// 포도주 10000개
+// 포도주의 양 1000 이하
+// 나올수있는 가장큰 값은 10000000
+// int 자료형정도면 충분할듯
+
+
+#include <iostream> 
+#include <cmath>
+using namespace std;
+int n = 0; int arr[10000]; int dp[2][10000] = { 0, }; int x = 0;
+int main()
+{
+	std::cin >> n;
+
+	for (int i = 0; i<n; i++)
+		std::cin >> arr[i];
+
+	// 첫번째잔
+	for (int i = 0; i < n; i++)
+	{
+		dp[0][i] = arr[i];
+
+	}
+	for (int i= 0; i< n + 1; i++)
+	{
+		// 0, 1는  한칸 점프를 포함함점프를 할수있음
+
+		// 2는 2칸점프부터 가능함
+		for (int j = 1; j < n - i + 1; j++)
+		{
+			for (int k = 0; k < 2; k++)
+			{
+				if (k == 1 && j == 1) continue;
+		
+				if (j == 1)
+				{
+					if (dp[k + 1][i + j] < dp[k][i] + arr[i + j])
+						dp[k + 1][i + j] = dp[k][i] + arr[i + j];
+				}
+				else if (j > 1)
+				{
+					if (dp[0][i + j] < dp[k][i] + arr[i + j])
+						dp[0][i + j] = dp[k][i] + arr[i + j];
+				}
+			}
+		}
+	}
+	//for (size_t j = 0; j < n; j++)
+	//{
+	//	cout << dp[0][j] << " " << dp[1][j] << " " << dp[2][j] << endl;;
+	//}
+
+	for (int i=0; i<3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			x = max(x, dp[i][n - j - 1]);
+		}
+
+	}
+	std::cout << x << endl;
+
+}
